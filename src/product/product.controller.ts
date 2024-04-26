09f7b12627f34f44b48cdb2f046932fcfac0ca32
resetmerlin <resetmerlin@gmail.com>
2024-04-26 21:27:20 +0900

    feat: Add findAllProduct logic

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
-import { GetProduct } from './dto/get-product.dto';
 
 @ApiTags('Product')
 @Controller('product')
@@ -9,7 +8,7 @@ export class ProductController {
   constructor(private productService: ProductService) {}
 
   @Get()
-  findAllProduct(@Body() request: GetProduct) {
-    console.log(request);
+  findAllProduct() {
+    return this.productService.findAllProduct();
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Product } from './schema/Product.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly productSchema: Product) {}
+  constructor(private readonly productModel: Model<Product>) {}
+
+  findAllProduct() {
+    return this.productModel.find({});
+  }
 }
